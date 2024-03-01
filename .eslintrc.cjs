ad1844227ae225bc22ef3b1095bc57fc34b22df6
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-03-01 16:48:53 +0200

    Added alias resolve for linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,15 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src']
+        ]
+      },
+    react: { version: '18.2' }
+  },
   plugins: ['react', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
