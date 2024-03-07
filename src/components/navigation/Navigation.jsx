66d8c32fbb3a12862fa07c447e6d68316147f838
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-03-07 13:24:28 +0200

    Fixed navbar in mobile mode

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -11,9 +11,9 @@ function Navigation({ onClick }) {
     <nav className="w-full fixed bg-background">
       <div className="container flex flex-row items-center justify-between h-16 space-y-0 py-4">
         <img src={LSLogo} className="mr-4 h-8" alt="LS Card Deck Logo" />
-        <h2 className="text-lg font-semibold whitespace-nowrap">Liberating Structures Card Deck</h2>
-        <div className="ml-auto flex w-full space-x-2 justify-end">
-          <Button variant="secondary" onClick={onClick}>
+        <h2 className="text-sm md:text-lg font-semibold whitespace-nowrap">Liberating Structures Card Deck</h2>
+        <div className="ml-2 md:ml-auto flex w-full space-x-2 justify-end">
+          <Button className="text-xs md:text-sm" variant="secondary" onClick={onClick}>
             About
           </Button>
         </div>
