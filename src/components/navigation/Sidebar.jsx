cda47654d370fbbced10190dc930519646c02de0
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-03-28 17:40:24 +0200

    Added sidebar component

---
+++ src/components/navigation/Sidebar.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { CircleIcon } from '@radix-ui/react-icons';
+
+import { Button } from '@/components/ui/button';
+
+import LSLogo from '../../data/logos/ls-menu.svg';
+
+function Sidebar({ onClick }) {
+  return (
+    <div className="border-r hidden md:block">
+      <div className="flex flex-col gap-5 h-full max-h-screen">
+        <div className="flex items-center border-b h-14 min-h-14 px-4 lg:h-[60px] lg:min-h-[60px] lg:px-6">
+          <a href="/" className="flex gap-5 font-semibold items-center">
+            <img src={LSLogo} className="h-8" alt="LS Card Deck Logo" />
+            <span>Liberating Structures Card Deck</span>
+          </a>
+        </div>
+        <div className="flex-1">
+          <nav className="grid gap-1 text-sm font-medium items-start px-2 lg:px-4">
+            <Button className="flex gap-5 justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('allCards')}>
+              <CircleIcon className="w-3 h-3" />
+              All Cards
+            </Button>
+            <Button className="flex gap-5 justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('reveal')}>
+              <span className="w-3 h-3 bg-green-500 rounded-full" />
+              Reveal
+            </Button>
+            <Button className="flex gap-5 justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('share')}>
+              <span className="w-3 h-3 bg-blue-500 rounded-full" />
+              Share
+            </Button>
+            <Button className="flex gap-5 justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('analyze')}>
+              <span className="w-3 h-3 bg-purple-500 rounded-full" />
+              Analyze
+            </Button>
+            <Button className="flex gap-5 justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('help')}>
+              <span className="w-3 h-3 bg-amber-700 rounded-full" />
+              Help
+            </Button>
+            <Button className="flex gap-5 justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('strategize')}>
+              <span className="w-3 h-3 bg-red-500 rounded-full" />
+              Strategize
+            </Button>
+            <Button className="flex gap-5 justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('plan')}>
+              <span className="w-3 h-3 bg-orange-500 rounded-full" />
+              Plan
+            </Button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Sidebar.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Sidebar;
