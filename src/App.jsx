ed950d1463eae3f20987d3250b902301c8498d7b
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-03-29 14:25:21 +0200

    Incorporated new sidebar and navigation in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { React, useState } from 'react';
 
 import Navigation from './components/navigation/Navigation';
+import Sidebar from './components/navigation/Sidebar';
 
 import CardModal from './components/card-modal/CardModal';
 import LSCardPreview from './components/ls-card/LSCardPreview';
@@ -13,32 +14,61 @@ function App() {
   // Index in deck array: card id - 1
   const [selectedCardData, setSelectedCardData] = useState(deckData[0]);
 
-  const handleAboutCardPreviewClick = () => {
-    // Set selected card data to NULL to show the about card
-    setSelectedCardData(null);
-    setOpenLSCardModal(true);
-  };
-
   const handleLSCardPreviewClick = (cardId) => {
     setSelectedCardData(deckData[cardId - 1]);
     setOpenLSCardModal(true);
   };
 
+  const handleSidebarClick = () => {
+    // TODO
+  };
+
+  const handleNavbarClick = (selectedCategory) => {
+    if (selectedCategory === 'about') {
+      // Set selected card data to NULL to show the about card
+      setSelectedCardData(null);
+      setOpenLSCardModal(true);
+      return;
+    }
+
+    handleSidebarClick(selectedCategory);
+  };
+
   return (
     <>
-      {/* Navbar */}
-      <Navigation onClick={handleAboutCardPreviewClick} />
-      {/* /Navbar */}
       {/* Modal */}
       <CardModal openModal={openLSCardModal} setOpenModal={setOpenLSCardModal} cardData={selectedCardData} />
       {/* /Modal */}
-      {/* Card Grid */}
-      <div className="flex flex-wrap gap-4 place-content-center items-start py-20 px-4">
-        {deckData.map((cardData) => (
-          <LSCardPreview key={cardData.id} data={cardData} onClick={handleLSCardPreviewClick} />
-        ))}
+      {/* Main page */}
+      <div className="grid w-full min-h-screen md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        {/* Sidebar */}
+        <Sidebar onClick={handleSidebarClick} />
+        {/* /Sidebar */}
+        {/* Main panel */}
+        <div className="flex flex-col">
+          {/* Navigation bar */}
+          <Navigation onClick={handleNavbarClick} />
+          {/* /Navigation bar */}
+          {/* Content */}
+          <main className="flex flex-col flex-1 p-4 lg:p-6">
+            {/* Title */}
+            <div className="flex items-center">
+              <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+            </div>
+            {/* /Title */}
+            {/* Card grid */}
+            <div className="flex flex-wrap gap-4 place-content-center items-start py-8">
+              {deckData.map((cardData) => (
+                <LSCardPreview key={cardData.id} data={cardData} onClick={handleLSCardPreviewClick} />
+              ))}
+            </div>
+            {/* /Card grid */}
+          </main>
+          {/* /Content */}
+        </div>
+        {/* /Main panel */}
       </div>
-      {/* /Card Grid */}
+      {/* /Main page */}
     </>
   );
 }
