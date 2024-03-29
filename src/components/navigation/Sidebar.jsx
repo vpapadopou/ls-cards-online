ce2a6ff09662308cdc83437f67a3fcdeaeb38b39
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-03-29 13:44:01 +0200

    Fix sidebar logo on md screens

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -11,12 +11,15 @@ function Sidebar({ onClick }) {
   return (
     <div className="border-r hidden md:block">
       <div className="flex flex-col gap-5 h-full max-h-screen">
+        {/* Logo */}
         <div className="flex items-center border-b h-14 min-h-14 px-4 lg:h-[60px] lg:min-h-[60px] lg:px-6">
-          <a href="/" className="flex gap-5 font-semibold items-center">
+          <a href="/" className="flex gap-5 text-sm font-semibold items-center lg:text-base">
             <img src={LSLogo} className="h-8" alt="LS Card Deck Logo" />
             <span>Liberating Structures Card Deck</span>
           </a>
         </div>
+        {/* /Logo */}
+        {/* Sidebar Content */}
         <div className="flex-1">
           <nav className="grid gap-1 text-sm font-medium items-start px-2 lg:px-4">
             <Button className="flex gap-5 justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('allCards')}>
@@ -49,6 +52,7 @@ function Sidebar({ onClick }) {
             </Button>
           </nav>
         </div>
+        {/* /Sidebar Content */}
       </div>
     </div>
   );
