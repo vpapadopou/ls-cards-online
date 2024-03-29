9878eb78c69b14e67dec6cf109addaf94564d5cd
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-03-29 13:58:33 +0200

    Updated Navigation component

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,25 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { CircleIcon, HamburgerMenuIcon, InfoCircledIcon } from '@radix-ui/react-icons';
+
 import { Button } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 
 import LSLogo from '../../data/logos/ls-menu.svg';
 
 function Navigation({ onClick }) {
   return (
-    <nav className="w-full fixed bg-background">
-      <div className="container flex flex-row items-center justify-between h-16 space-y-0 py-4">
-        <img src={LSLogo} className="mr-4 h-8" alt="LS Card Deck Logo" />
-        <h2 className="text-sm md:text-lg font-semibold whitespace-nowrap">Liberating Structures Card Deck</h2>
-        <div className="ml-2 md:ml-auto flex w-full space-x-2 justify-end">
-          <Button className="text-xs md:text-sm" variant="secondary" onClick={onClick}>
-            About
+    <header className="flex gap-5 h-14 items-center px-4 border-b lg:h-[60px] lg:px-6">
+      {/* Side sheet (shown on small screens) */}
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className="shrink-0 md:hidden" size="icon" variant="outline">
+            <HamburgerMenuIcon className="w-5 h-5" />
+            <span className="sr-only">Toggle navigation menu</span>
           </Button>
-        </div>
-      </div>
-      <Separator />
-    </nav>
+        </SheetTrigger>
+        <SheetContent className="flex flex-col w-[300px]" side="left">
+          <nav className="grid gap-2 text-md font-medium">
+            {/* Logo */}
+            <div className="flex gap-5 text-lg font-semibold items-center mb-2">
+              <img src={LSLogo} className="h-8" alt="LS Card Deck Logo" />
+              <span>Liberating Structures Card Deck</span>
+            </div>
+            {/* /Logo */}
+            {/* Sheet content */}
+            <Button className="flex gap-5 text-md justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('allCards')}>
+              <CircleIcon className="w-3 h-3" />
+              All Cards
+            </Button>
+            <Button className="flex gap-5 text-md justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('reveal')}>
+              <span className="w-3 h-3 bg-green-500 rounded-full" />
+              Reveal
+            </Button>
+            <Button className="flex gap-5 text-md justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('share')}>
+              <span className="w-3 h-3 bg-blue-500 rounded-full" />
+              Share
+            </Button>
+            <Button className="flex gap-5 text-md justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('analyze')}>
+              <span className="w-3 h-3 bg-purple-500 rounded-full" />
+              Analyze
+            </Button>
+            <Button className="flex gap-5 text-md justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('help')}>
+              <span className="w-3 h-3 bg-amber-700 rounded-full" />
+              Help
+            </Button>
+            <Button className="flex gap-5 text-md justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('strategize')}>
+              <span className="w-3 h-3 bg-red-500 rounded-full" />
+              Strategize
+            </Button>
+            <Button className="flex gap-5 text-md justify-start items-center px-3 py-2" variant="ghost" onClick={() => onClick('plan')}>
+              <span className="w-3 h-3 bg-orange-500 rounded-full" />
+              Plan
+            </Button>
+            {/* /Sheet content */}
+          </nav>
+        </SheetContent>
+      </Sheet>
+      {/* /Side sheet (shown on small screens) */}
+      {/* Navbar */}
+      <div className="flex-1 w-full">{/* Content to be added */}</div>
+      <Button size="icon" variant="outline" onClick={() => onClick('about')}>
+        <InfoCircledIcon className="w-5 h-5" />
+        <span className="sr-only">About</span>
+      </Button>
+      {/* /Navbar */}
+    </header>
   );
 }
 
