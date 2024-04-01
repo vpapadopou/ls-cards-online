e6132a397b7789fc63f30e27706ee489bb09ec7d
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-04-01 18:04:24 +0300

    Updated App component to tie everything together

--- src/App.jsx
+++ src/App.jsx
@@ -3,73 +3,46 @@ import { React, useState } from 'react';
 import Navigation from './components/navigation/Navigation';
 import Sidebar from './components/navigation/Sidebar';
 
+import CardDeck from './components/card-deck/CardDeck';
 import CardModal from './components/card-modal/CardModal';
-import LSCardPreview from './components/ls-card/LSCardPreview';
 
 import deckData from './data/cards';
 
 function App() {
-  const [openLSCardModal, setOpenLSCardModal] = useState(false);
-  // Selected card data to be shown in the modal
-  // Index in deck array: card id - 1
-  const [selectedCardData, setSelectedCardData] = useState(deckData[0]);
+  const [openCardModal, setOpenCardModal] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(deckData[0]);
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
-  const handleLSCardPreviewClick = (cardId) => {
-    setSelectedCardData(deckData[cardId - 1]);
-    setOpenLSCardModal(true);
-  };
-
-  const handleSidebarClick = () => {
-    // TODO
-  };
-
-  const handleNavbarClick = (selectedCategory) => {
-    if (selectedCategory === 'about') {
-      // Set selected card data to NULL to show the about card
-      setSelectedCardData(null);
-      setOpenLSCardModal(true);
-      return;
-    }
-
-    handleSidebarClick(selectedCategory);
+  const handleModalClick = (cardData) => {
+    setSelectedCard(cardData);
+    setOpenCardModal(true);
   };
 
   return (
-    <>
+    <div>
       {/* Modal */}
-      <CardModal openModal={openLSCardModal} setOpenModal={setOpenLSCardModal} cardData={selectedCardData} />
+      <CardModal openModal={openCardModal} setOpenModal={setOpenCardModal} cardData={selectedCard} />
       {/* /Modal */}
       {/* Main page */}
       <div className="grid w-full min-h-screen md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
         {/* Sidebar */}
-        <Sidebar onClick={handleSidebarClick} />
+        <Sidebar onCategoryClick={setSelectedCategory} />
         {/* /Sidebar */}
         {/* Main panel */}
         <div className="flex flex-col">
           {/* Navigation bar */}
-          <Navigation onClick={handleNavbarClick} />
+          <Navigation onCategoryClick={setSelectedCategory} onAboutClick={handleModalClick} />
           {/* /Navigation bar */}
           {/* Content */}
           <main className="flex flex-col flex-1 p-4 lg:p-6">
-            {/* Title */}
-            <div className="flex items-center">
-              <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-            </div>
-            {/* /Title */}
-            {/* Card grid */}
-            <div className="flex flex-wrap gap-4 place-content-center items-start py-8">
-              {deckData.map((cardData) => (
-                <LSCardPreview key={cardData.id} data={cardData} onClick={handleLSCardPreviewClick} />
-              ))}
-            </div>
-            {/* /Card grid */}
+            <CardDeck cardList={deckData} selectedCategory={selectedCategory} onClick={handleModalClick} />
           </main>
           {/* /Content */}
         </div>
         {/* /Main panel */}
       </div>
       {/* /Main page */}
-    </>
+    </div>
   );
 }
 
