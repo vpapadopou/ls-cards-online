81c5efe006d822045c8c29567764afa6df254a4a
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-04-03 21:51:16 +0300

    Added category index

--- src/data/card-categories.js
+++ src/data/card-categories.js
@@ -1,3 +1,5 @@
+import cardList from './cards';
+
 export const categoryList = [
   { id: 1, title: 'Reveal', color: 'bg-green-500' },
   { id: 2, title: 'Share', color: 'bg-blue-500' },
@@ -6,5 +8,19 @@ export const categoryList = [
   { id: 5, title: 'Strategize', color: 'bg-red-500' },
   { id: 6, title: 'Plan', color: 'bg-orange-500' },
 ];
+
+// Create an initial object with each card's id as key and an empty array as value
+// i.e. { 1: [], 2: [], .... }
+const emptyIndex = categoryList.reduce((accumulator, category) => {
+  accumulator[category.id] = [];
+  return accumulator;
+}, {});
+
+// Populate the object by adding each card's id to the appropriate array based on
+// its category/ies
+export const categoryIndex = cardList.reduce((accumulator, card) => {
+  card.categories.forEach((cardCategory) => accumulator[cardCategory].push(card.id));
+  return accumulator;
+}, emptyIndex);
 
 export default categoryList;
--- src/data/card-categories.test.js
+++ src/data/card-categories.test.js
@@ -1,9 +1,33 @@
 import { describe, expect, test } from 'vitest';
 
-import { categoryList } from './card-categories';
+import { categoryIndex, categoryList } from './card-categories';
 
 describe('Data: Card Categories', () => {
   test('should have a category list with 6 categories', () => {
     expect(categoryList.length).toEqual(6);
+  });
+
+  test('should have a category index with 6 keys', () => {
+    expect(Object.keys(categoryIndex).length).toEqual(6);
+  });
+
+  test('should have 12 card ids in category 1', () => {
+    expect(categoryIndex[1].length).toEqual(12);
+  });
+
+  test('should have 7 card ids in category 2', () => {
+    expect(categoryIndex[2].length).toEqual(7);
+  });
+  test('should have 7 card ids in category 3', () => {
+    expect(categoryIndex[3].length).toEqual(7);
+  });
+  test('should have 6 card ids in category 4', () => {
+    expect(categoryIndex[4].length).toEqual(6);
+  });
+  test('should have 5 card ids in category 5', () => {
+    expect(categoryIndex[5].length).toEqual(5);
+  });
+  test('should have 2 card ids in category 6', () => {
+    expect(categoryIndex[6].length).toEqual(2);
   });
 });
