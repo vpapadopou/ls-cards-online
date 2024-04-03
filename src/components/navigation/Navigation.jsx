044d3a021ac47a2a5f5c7c204f8123a50c58e490
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-04-03 18:49:35 +0300

    Close sheet when a category is selected

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { CircleIcon, HamburgerMenuIcon, InfoCircledIcon } from '@radix-ui/react-icons';
@@ -9,10 +9,20 @@ import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import LSLogo from '@/data/ls-logo.svg';
 
 function Navigation({ onAboutClick, onCategoryClick }) {
+  const [openSheet, setOpenSheet] = useState(false);
+
+  // Call the appropriate callback and close the sheet.
+  // Used to programmatically close the sheet after user selects a category.
+  // Otherwise, the data in the background changes and the sheet remains open.
+  const selectCategory = (category) => {
+    onCategoryClick(category);
+    setOpenSheet(false);
+  };
+
   return (
     <header className="flex gap-5 h-14 sticky top-0 bg-background items-center px-4 border-b lg:h-[60px] lg:px-6">
       {/* Side sheet (shown on small screens) */}
-      <Sheet>
+      <Sheet open={openSheet} onOpenChange={setOpenSheet}>
         <SheetTrigger asChild>
           <Button className="shrink-0 md:hidden" size="icon" variant="outline" data-testid="sheet-toggle-button">
             <HamburgerMenuIcon className="w-5 h-5" />
@@ -23,7 +33,7 @@ function Navigation({ onAboutClick, onCategoryClick }) {
           <nav className="grid gap-2 text-md font-medium">
             {/* Logo */}
             <div className="flex gap-5 text-lg font-semibold items-center mb-2">
-              <img src={LSLogo} className="h-8" alt="LS Card Deck Logo" />
+              <img src={LSLogo} className="h-8" alt="Liberating Structures Card Deck Logo" />
               <span>Liberating Structures Card Deck</span>
             </div>
             {/* /Logo */}
@@ -31,7 +41,7 @@ function Navigation({ onAboutClick, onCategoryClick }) {
             <Button
               className="flex gap-5 text-md justify-start items-center px-3 py-2"
               variant="ghost"
-              onClick={() => onCategoryClick('All')}
+              onClick={() => selectCategory('All')}
               data-testid="category-button"
             >
               <CircleIcon className="w-3 h-3" />
@@ -40,7 +50,7 @@ function Navigation({ onAboutClick, onCategoryClick }) {
             <Button
               className="flex gap-5 text-md justify-start items-center px-3 py-2"
               variant="ghost"
-              onClick={() => onCategoryClick('Reveal')}
+              onClick={() => selectCategory('Reveal')}
               data-testid="category-button"
             >
               <span className="w-3 h-3 bg-green-500 rounded-full" />
@@ -49,7 +59,7 @@ function Navigation({ onAboutClick, onCategoryClick }) {
             <Button
               className="flex gap-5 text-md justify-start items-center px-3 py-2"
               variant="ghost"
-              onClick={() => onCategoryClick('Share')}
+              onClick={() => selectCategory('Share')}
               data-testid="category-button"
             >
               <span className="w-3 h-3 bg-blue-500 rounded-full" />
@@ -58,7 +68,7 @@ function Navigation({ onAboutClick, onCategoryClick }) {
             <Button
               className="flex gap-5 text-md justify-start items-center px-3 py-2"
               variant="ghost"
-              onClick={() => onCategoryClick('Analyze')}
+              onClick={() => selectCategory('Analyze')}
               data-testid="category-button"
             >
               <span className="w-3 h-3 bg-purple-500 rounded-full" />
@@ -67,7 +77,7 @@ function Navigation({ onAboutClick, onCategoryClick }) {
             <Button
               className="flex gap-5 text-md justify-start items-center px-3 py-2"
               variant="ghost"
-              onClick={() => onCategoryClick('Help')}
+              onClick={() => selectCategory('Help')}
               data-testid="category-button"
             >
               <span className="w-3 h-3 bg-amber-700 rounded-full" />
@@ -76,7 +86,7 @@ function Navigation({ onAboutClick, onCategoryClick }) {
             <Button
               className="flex gap-5 text-md justify-start items-center px-3 py-2"
               variant="ghost"
-              onClick={() => onCategoryClick('Strategize')}
+              onClick={() => selectCategory('Strategize')}
               data-testid="category-button"
             >
               <span className="w-3 h-3 bg-red-500 rounded-full" />
@@ -85,7 +95,7 @@ function Navigation({ onAboutClick, onCategoryClick }) {
             <Button
               className="flex gap-5 text-md justify-start items-center px-3 py-2"
               variant="ghost"
-              onClick={() => onCategoryClick('Plan')}
+              onClick={() => selectCategory('Plan')}
               data-testid="category-button"
             >
               <span className="w-3 h-3 bg-orange-500 rounded-full" />
@@ -101,7 +111,7 @@ function Navigation({ onAboutClick, onCategoryClick }) {
       {/* On click send NULL back so the about modal opens */}
       <Button size="icon" variant="outline" onClick={() => onAboutClick(null)} data-testid="about-button">
         <InfoCircledIcon className="w-5 h-5" />
-        <span className="sr-only">About</span>
+        <span className="sr-only">Open about modal</span>
       </Button>
       {/* /Navbar */}
     </header>
