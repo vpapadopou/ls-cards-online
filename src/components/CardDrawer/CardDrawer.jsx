1bc955cfe1f5a05315de54c2455c9f9cd3c67c26
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-04-16 23:38:24 +0300

    CardDrawer now uses store to get card data

--- src/components/CardDrawer/CardDrawer.jsx
+++ src/components/CardDrawer/CardDrawer.jsx
@@ -4,10 +4,16 @@ import PropTypes from 'prop-types';
 import { Drawer, DrawerContent } from '@/components/ui/drawer';
 import { ScrollArea } from '@/components/ui/scroll-area';
 
-import AboutCard from '../AboutCard/AboutCard';
+import { useCardStore } from '@/hooks/use-card-store';
+
+import { getCardById } from '@/services/cards';
+
 import LSCard from '../LSCard/LSCard';
 
-function CardDrawer({ openDrawer, setOpenDrawer, cardData }) {
+function CardDrawer({ openDrawer, setOpenDrawer }) {
+  const selectedCardId = useCardStore((state) => state.selectedCardId);
+  const cardData = getCardById(selectedCardId);
+
   return (
     <Drawer
       open={openDrawer}
@@ -15,11 +21,13 @@ function CardDrawer({ openDrawer, setOpenDrawer, cardData }) {
         setOpenDrawer(open);
       }}
     >
-      {/* Max drawer width set acconding to Material 3 guidelines */}
+      {/* Max drawer width set to 640px acconding to Material 3 guidelines */}
       <DrawerContent className="max-w-[640px]">
         <ScrollArea className="h-[80vh]">
           <div className="flex flex-col items-center p-8">
-            <div className="max-w-sm">{cardData ? <LSCard data={cardData} /> : <AboutCard />}</div>
+            <div className="max-w-sm">
+              <LSCard data={cardData} />
+            </div>
           </div>
         </ScrollArea>
       </DrawerContent>
@@ -28,32 +36,8 @@ function CardDrawer({ openDrawer, setOpenDrawer, cardData }) {
 }
 
 CardDrawer.propTypes = {
-  cardData: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    logo: PropTypes.node.isRequired,
-    categories: PropTypes.arrayOf(PropTypes.number).isRequired,
-    time: PropTypes.number.isRequired,
-    invitation: PropTypes.string,
-    people: PropTypes.string.isRequired,
-    spaceAndMaterials: PropTypes.string.isRequired,
-    stepsAltTitle: PropTypes.string,
-    steps: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        description: PropTypes.string.isRequired,
-        time: PropTypes.string,
-      })
-    ).isRequired,
-    stringWith: PropTypes.string,
-  }),
   openDrawer: PropTypes.bool.isRequired,
   setOpenDrawer: PropTypes.func.isRequired,
-};
-
-CardDrawer.defaultProps = {
-  cardData: null,
 };
 
 export default CardDrawer;
--- src/components/CardDrawer/CardDrawer.test.jsx
+++ src/components/CardDrawer/CardDrawer.test.jsx
@@ -1,52 +1,27 @@
 import { describe, expect, test, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
+import cardList from '@/data/cards';
+
 import CardDrawer from './CardDrawer';
 
 describe('Component: CardDrawer', () => {
-  const testCardData = {
-    id: 1,
-    title: 'Testing Title',
-    description: 'Testing Description',
-    logo: 'testing_path',
-    categories: [1, 2],
-    time: 20,
-    invitation: 'Testing Invitation',
-    people: 'Testing People',
-    spaceAndMaterials: 'Testing Space and Materials',
-    steps: [{ id: 1, description: 'Test Step 1' }],
-    stepsAltTitle: 'Testing Alternate Steps Title',
-    stringWith: 'Testing String With',
-  };
-
+  // Default selected card in the store is the first on the list
+  const selectedCard = cardList[0];
   // Used by drawer animation - stub it to avoid errors
   window.scrollTo = vi.fn();
 
-  test('should call LSCard component when the drawer is open and Card Data is passed. Diplay the card title text inside <h4>', () => {
+  test('should call LSCard component when the drawer is open. Diplay the card title text inside <h4>', () => {
     const stub = vi.fn();
-    render(<CardDrawer cardData={testCardData} openDrawer setOpenDrawer={stub} />);
+    render(<CardDrawer openDrawer setOpenDrawer={stub} />);
 
-    expect(screen.getByText('Testing Title', { selector: 'h4' })).toBeDefined();
+    expect(screen.getByText(selectedCard.title, { selector: 'h4' })).toBeDefined();
   });
 
-  test('should call AboutCard component when the drawer is open and Card Data is NOT passed. Diplay the card title text inside <h4>', () => {
+  test('should NOT diplay the card title text inside <h4> when the drawer is closed', () => {
     const stub = vi.fn();
-    render(<CardDrawer cardData={null} openDrawer setOpenDrawer={stub} />);
+    render(<CardDrawer openDrawer={false} setOpenDrawer={stub} />);
 
-    expect(screen.getByText('Liberating Structures Card Deck', { selector: 'h4' })).toBeDefined();
-  });
-
-  test('should NOT diplay the card title text inside <h4> when the drawer is closed. Card Data is passed', () => {
-    const stub = vi.fn();
-    render(<CardDrawer cardData={testCardData} openDrawer={false} setOpenDrawer={stub} />);
-
-    expect(screen.queryByText('Testing Title', { selector: 'h4' })).toBeNull();
-  });
-
-  test('should NOT diplay the card title text inside <h4> when the drawer is closed. Card Data is NOT passed', () => {
-    const stub = vi.fn();
-    render(<CardDrawer cardData={null} openDrawer={false} setOpenDrawer={stub} />);
-
-    expect(screen.queryByText('Liberating Structures Card Deck', { selector: 'h4' })).toBeNull();
+    expect(screen.queryByText(selectedCard.title, { selector: 'h4' })).toBeNull();
   });
 });
