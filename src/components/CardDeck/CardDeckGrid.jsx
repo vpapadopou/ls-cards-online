bfa008143f7be45774d820877ceaba0d32c71dcb
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-04-17 20:12:12 +0300

    Added CardDeckGrid component

---
+++ src/components/CardDeck/CardDeckGrid.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import LSCardPreview from '@/components/LSCard/LSCardPreview';
+import { ScrollArea } from '@/components/ui/scroll-area';
+
+import { useCardStore } from '@/hooks/use-card-store';
+
+import { getCardsByCategory } from '@/services/cards';
+
+function CardDeckGrid({ onClick }) {
+  const selectedCategoryId = useCardStore((state) => state.selectedCategoryId);
+  const cards = getCardsByCategory(selectedCategoryId);
+
+  return (
+    // Scroll height is screen minus navbar minus approx 6rem for the category title
+    <ScrollArea className="h-[calc(100vh-60px-6rem)]">
+      <div className="flex flex-wrap gap-4 place-content-center items-start px-4 pb-4">
+        {cards.map((card) => (
+          <LSCardPreview key={card.id} data={card} onClick={onClick} />
+        ))}
+      </div>
+    </ScrollArea>
+  );
+}
+
+CardDeckGrid.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default CardDeckGrid;
---
+++ src/components/CardDeck/CardDeckGrid.test.jsx
@@ -0,0 +1,27 @@
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import deckData from '@/data/cards';
+
+import CardDeckGrid from './CardDeckGrid';
+
+describe('Component: CardDeck', () => {
+  const stub = vi.fn();
+
+  beforeEach(() => {
+    render(<CardDeckGrid onClick={stub} />);
+  });
+
+  test('should display as many card previews as the cards in the deck', () => {
+    expect(screen.getAllByTestId('card-preview').length).toEqual(deckData.length);
+  });
+
+  test("should call the provided callback function AND pass the card's id when a card preview is clicked", () => {
+    // Click the Impromptu Networking card (the second one in the grid)
+    const card = screen.getAllByTestId('card-preview')[1];
+    fireEvent.click(card);
+
+    const cardData = deckData[1];
+    expect(stub).toHaveBeenCalledWith(cardData.id);
+  });
+});
