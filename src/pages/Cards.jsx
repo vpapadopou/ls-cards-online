176f78e16ee7051712011c8f84ed187925d30778
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-04-24 21:48:25 +0300

    Removed selected category title

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -5,24 +5,12 @@ import CardDeckDetails from '@/components/CardDeck/CardDeckDetails';
 
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 
-import { useCardStore } from '@/hooks/use-card-store';
-
-import { getCategoryById } from '@/services/card-categories';
-
 function Cards() {
-  const selectedCategoryId = useCardStore((state) => state.selectedCategoryId);
-  const { title: selectedCategoryTitle } = getCategoryById(selectedCategoryId);
-
   return (
     <ResizablePanelGroup direction="horizontal">
       {/* Left panel (card grid) */}
       <ResizablePanel defaultSize={65}>
         <div className="flex flex-col">
-          {/* Selected category title */}
-          <div className="px-4 py-8">
-            <h1 className="text-lg font-semibold md:text-2xl">{selectedCategoryTitle}</h1>
-          </div>
-          {/* /Selected category title */}
           {/* Card deck */}
           <CardDeck />
           {/* /Card deck */}
