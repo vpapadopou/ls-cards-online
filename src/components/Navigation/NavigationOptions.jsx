a33532a1b17e28135d67d69f491a64a87747f4af
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-06-20 13:04:22 +0300

    Added navigation options component

---
+++ src/components/Navigation/NavigationOptions.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { SunIcon, MoonIcon } from '@radix-ui/react-icons';
+
+import { Button } from '@/components/ui/button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+
+import { useTheme } from '@/providers/theme-provider';
+
+function NavigationOptions() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="w-10 md:w-9" variant="outline" size="icon">
+          <SunIcon className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default NavigationOptions;
