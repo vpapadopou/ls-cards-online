4e39392e98aea1ec9e1c8cba7c5cf6a790651d20
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-06-20 17:29:14 +0300

    Modified svg logos to work with dark mode

--- src/data/cards/logos/OneTwoFourAll.jsx
+++ src/data/cards/logos/OneTwoFourAll.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 function OneTwoFourAllSvg(props) {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.71 25.466" {...props}>
+    <svg viewBox="0 0 24.71 25.466" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g transform="translate(-22.584 -105.98)">
-        <circle cx={24.427} cy={116.35} r={1.843} />
-        <circle cx={24.946} cy={122.84} r={1.843} />
-        <circle cx={28.915} cy={127.8} r={1.843} />
-        <circle cx={35.057} cy={129.6} r={1.843} />
-        <circle cx={45.404} cy={122.23} r={1.843} />
-        <circle cx={45.452} cy={115.57} r={1.843} />
-        <circle cx={34.963} cy={107.82} r={1.843} />
-        <path d="m24.518 109.66 5.746-.628-2.725 5.104.08-3.314zM40.914 106.18l2.381 5.267-5.698-1.005 3.175-.954zM45.05 128.63l-5.78.073 3.203-4.818-.397 3.29z" />
+        <g className="fill-black dark:fill-white stroke-none">
+          <circle cx="24.427" cy="116.35" r="1.8426" />
+          <circle cx="24.946" cy="122.84" r="1.8426" />
+          <circle cx="28.915" cy="127.8" r="1.8426" />
+          <circle cx="35.057" cy="129.6" r="1.8426" />
+          <circle cx="45.404" cy="122.23" r="1.8426" />
+          <circle cx="45.452" cy="115.57" r="1.8426" />
+          <circle cx="34.963" cy="107.82" r="1.8426" />
+          <path d="m24.518 109.66 5.7459-.62797-2.7249 5.1038.07923-3.3136z" />
+          <path d="m40.914 106.18 2.3811 5.2669-5.6977-1.0053 3.1745-.95364z" />
+          <path d="m45.05 128.63-5.7797.0733 3.2024-4.8186-.39706 3.2907z" />
+        </g>
       </g>
     </svg>
   );
--- src/data/cards/logos/Panarchy.jsx
+++ src/data/cards/logos/Panarchy.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 function PanarchySvg(props) {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.429 47.429" {...props}>
+    <svg viewBox="0 0 47.429 47.429" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g transform="translate(-343.59 -137.27)">
-        <circle cx={367.3} cy={160.98} r={23.715} />
-        <circle cx={365.52} cy={164.54} r={18.462} fill="#fff" />
-        <circle cx={363.09} cy={168.09} r={15.348} />
-        <circle cx={361.13} cy={171.09} r={10.514} fill="#fff" />
-        <circle cx={359.26} cy={174.36} r={7.261} />
+        <g className="fill-black dark:fill-white stroke-none">
+          <circle cx="367.3" cy="160.98" r="23.715" />
+          <circle className="fill-white dark:fill-black" cx="365.52" cy="164.54" r="18.462" />
+          <circle cx="363.09" cy="168.09" r="15.348" />
+          <circle className="fill-white dark:fill-black" cx="361.13" cy="171.09" r="10.514" />
+          <circle cx="359.26" cy="174.36" r="7.2606" />
+        </g>
       </g>
     </svg>
   );
