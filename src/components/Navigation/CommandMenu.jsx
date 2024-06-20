99a8f56da27b90aadfb3e471929569fc4c811692
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-06-20 13:05:00 +0300

    Adjusted CommandMenu to look better on screen

--- src/components/Navigation/CommandMenu.jsx
+++ src/components/Navigation/CommandMenu.jsx
@@ -47,11 +47,11 @@ function CommandMenu() {
     <>
       <Button
         variant="outline"
-        className="relative h-8 w-full justify-start rounded-[0.5rem] bg-background text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-52 lg:w-80"
+        className="relative h-9 w-full justify-start rounded-[0.5rem] bg-background text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-52 lg:w-80"
         onClick={() => setOpenCommandMenu(true)}
       >
         <span className="inline-flex">Search...</span>
-        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-6 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
           /
         </kbd>
       </Button>
