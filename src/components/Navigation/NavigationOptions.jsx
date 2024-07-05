06925d35c73920d373b1c00a1465f4e1b9e248d8
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-07-05 10:49:12 +0300

    Added full menu and options in NavigationOptions component

--- src/components/Navigation/NavigationOptions.jsx
+++ src/components/Navigation/NavigationOptions.jsx
@@ -1,28 +1,62 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { SunIcon, MoonIcon } from '@radix-ui/react-icons';
+import { GitHubLogoIcon, GlobeIcon, InfoCircledIcon, MoonIcon, SunIcon, DotsVerticalIcon } from '@radix-ui/react-icons';
 
 import { Button } from '@/components/ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 import { useTheme } from '@/providers/theme-provider';
 
 function NavigationOptions() {
+  const navigate = useNavigate();
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button className="w-10 md:w-9" variant="outline" size="icon">
-          <SunIcon className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <DotsVerticalIcon className="w-4 h-4" />
+          <span className="sr-only">Options</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        {/* <DropdownMenuSeparator /> */}
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          <SunIcon className="mr-2" />
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          <MoonIcon className="mr-2" />
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          <GlobeIcon className="mr-2" />
+          System
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <a href="https://github.com/vpapadopou/ls-cards-online" target="_blank" rel="noopener noreferrer">
+            <GitHubLogoIcon className="inline-block align-baseline mr-2" />
+            GitHub
+          </a>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => {
+            navigate('/about');
+          }}
+        >
+          <InfoCircledIcon className="mr-2" />
+          About
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
