a3256ec5cb21c140fd0dc0d4af34b9e2466f6658
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-08-22 13:29:14 +0300

    Added popover for filtering

--- src/components/CardDeck/CardDeckOptions.jsx
+++ src/components/CardDeck/CardDeckOptions.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import { React, useState } from 'react';
 
 import { SortAscending } from '@phosphor-icons/react';
+import { CircleIcon } from '@radix-ui/react-icons';
 
+import { Button } from '@/components/ui/button';
+import { Command, CommandGroup, CommandItem, CommandList } from '@/components/ui/command';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import { useCardStore } from '@/hooks/use-card-store';
 
 import { getSortingOptions } from '@/services/cards';
+import { getAllCategories, getCategoryById } from '@/services/card-categories';
 
 function CardDeckOptions() {
+  const selectedCategoryId = useCardStore((state) => state.selectedCategoryId);
+  const setSelectedCategoryId = useCardStore((state) => state.setSelectedCategoryId);
+  const [openCategoryFilter, setOpenCategoryFilter] = useState(false);
+
   const selectedSortingId = useCardStore((state) => state.selectedSortingId);
   const setSelectedSortingId = useCardStore((state) => state.setSelectedSortingId);
   const sortingOptions = getSortingOptions();
 
+  const handleCategorySelection = (categoryId) => {
+    setSelectedCategoryId(categoryId);
+    setOpenCategoryFilter(false);
+  };
+
   return (
-    <div className="flex flex-row px-4 py-6">
+    <div className="flex flex-row gap-4 px-4 py-6">
       {/* Tab Selection */}
       <TabsList>
         <TabsTrigger value="cardView" data-testid="tab-trigger">
@@ -26,6 +40,46 @@ function CardDeckOptions() {
         </TabsTrigger>
       </TabsList>
       {/* /Tab Selection */}
+      {/* Category Selection */}
+      <Popover open={openCategoryFilter} onOpenChange={setOpenCategoryFilter}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" className="w-[150px] justify-start">
+            {selectedCategoryId ? <span className={`w-3 h-3 ${getCategoryById(selectedCategoryId).color} rounded-full mr-2 inline-block`} /> : null}
+            {selectedCategoryId ? getCategoryById(selectedCategoryId).title : 'All Categories'}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0" side="bottom" align="start">
+          <Command>
+            <CommandList>
+              <CommandGroup>
+                <CommandItem
+                  key={0}
+                  value={0}
+                  onSelect={() => {
+                    handleCategorySelection(0);
+                  }}
+                >
+                  <CircleIcon className="w-3 h-3 mr-2 inline-block" />
+                  <span>All Categories</span>
+                </CommandItem>
+                {getAllCategories().map((cardCategory) => (
+                  <CommandItem
+                    key={cardCategory.id}
+                    value={cardCategory.id}
+                    onSelect={() => {
+                      handleCategorySelection(cardCategory.id);
+                    }}
+                  >
+                    <span className={`w-3 h-3 ${cardCategory.color} rounded-full mr-2 inline-block`} />
+                    <span>{cardCategory.title}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+      {/* /Category Selection */}
       {/* Sorting Selection */}
       <div className="ml-auto">
         <Select defaultValue="id" value={selectedSortingId} onValueChange={setSelectedSortingId} data-testid="sorting-trigger">
