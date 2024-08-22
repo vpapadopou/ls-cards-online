62eac4658919bd22546fae7e43e577964e053c7a
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-08-22 15:20:49 +0300

    Added popover for sorting

--- src/components/CardDeck/CardDeckOptions.jsx
+++ src/components/CardDeck/CardDeckOptions.jsx
@@ -1,12 +1,10 @@
 import { React, useState } from 'react';
 
-import { SortAscending } from '@phosphor-icons/react';
 import { CircleIcon } from '@radix-ui/react-icons';
 
 import { Button } from '@/components/ui/button';
 import { Command, CommandGroup, CommandItem, CommandList } from '@/components/ui/command';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import { useCardStore } from '@/hooks/use-card-store';
@@ -17,15 +15,22 @@ import { getAllCategories, getCategoryById } from '@/services/card-categories';
 function CardDeckOptions() {
   const selectedCategoryId = useCardStore((state) => state.selectedCategoryId);
   const setSelectedCategoryId = useCardStore((state) => state.setSelectedCategoryId);
+  const selectedCategory = getCategoryById(selectedCategoryId);
   const [openCategoryFilter, setOpenCategoryFilter] = useState(false);
 
   const selectedSortingId = useCardStore((state) => state.selectedSortingId);
   const setSelectedSortingId = useCardStore((state) => state.setSelectedSortingId);
   const sortingOptions = getSortingOptions();
+  const [openSortingFilter, setOpenSortingFilter] = useState(false);
 
   const handleCategorySelection = (categoryId) => {
     setSelectedCategoryId(categoryId);
     setOpenCategoryFilter(false);
+  };
+
+  const handleSortingSelection = (sortingId) => {
+    setSelectedSortingId(sortingId);
+    setOpenSortingFilter(false);
   };
 
   return (
@@ -43,15 +48,16 @@ function CardDeckOptions() {
       {/* Category Selection */}
       <Popover open={openCategoryFilter} onOpenChange={setOpenCategoryFilter}>
         <PopoverTrigger asChild>
-          <Button variant="outline" className="w-[150px] justify-start">
-            {selectedCategoryId ? <span className={`w-3 h-3 ${getCategoryById(selectedCategoryId).color} rounded-full mr-2 inline-block`} /> : null}
-            {selectedCategoryId ? getCategoryById(selectedCategoryId).title : 'All Categories'}
+          <Button variant="outline" className="justify-start">
+            {!!selectedCategoryId && <span className={`w-3 h-3 ${selectedCategory.color} rounded-full mr-2 inline-block`} />}
+            {selectedCategoryId ? selectedCategory.title : 'All Categories'}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="p-0" side="bottom" align="start">
           <Command>
             <CommandList>
               <CommandGroup>
+                {/* All cards is a special option with id 0 */}
                 <CommandItem
                   key={0}
                   value={0}
@@ -81,32 +87,32 @@ function CardDeckOptions() {
       </Popover>
       {/* /Category Selection */}
       {/* Sorting Selection */}
-      <div className="ml-auto">
-        <Select defaultValue="id" value={selectedSortingId} onValueChange={setSelectedSortingId} data-testid="sorting-trigger">
-          <SelectTrigger data-testid="sorting-option-trigger">
-            <SelectValue placeholder="Sort By">
-              <SortAscending className="w-4 h-4" />
-              <span className="sr-only">Select sorting method</span>
-            </SelectValue>
-          </SelectTrigger>
-          <SelectContent align="end">
-            <SelectGroup
-              // Ref hack used to stop click propagation behind the select
-              ref={(ref) =>
-                ref?.addEventListener('touchend', (e) => {
-                  e.preventDefault();
-                })
-              }
-            >
-              {sortingOptions.map((sortingOption) => (
-                <SelectItem key={sortingOption.value} value={sortingOption.value} data-testid="sorting-option">
-                  {sortingOption.name}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
+      <Popover open={openSortingFilter} onOpenChange={setOpenSortingFilter}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" className="justify-start">
+            {sortingOptions[selectedSortingId].name}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0" side="bottom" align="start">
+          <Command>
+            <CommandList>
+              <CommandGroup>
+                {sortingOptions.map((sortingOption) => (
+                  <CommandItem
+                    key={sortingOption.value}
+                    value={sortingOption.value}
+                    onSelect={() => {
+                      handleSortingSelection(sortingOption.value);
+                    }}
+                  >
+                    <span>{sortingOption.name}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
       {/* /Sorting Selection */}
     </div>
   );
