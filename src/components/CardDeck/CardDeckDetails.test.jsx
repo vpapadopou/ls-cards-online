aedfbef41fd125ad27e3d9aed2840e7fd0b1b800
Vasileios Papadopoulos <vspapadopou@gmail.com>
2024-08-23 23:00:19 +0300

    Updated tests

--- src/components/CardDeck/CardDeckDetails.test.jsx
+++ src/components/CardDeck/CardDeckDetails.test.jsx
@@ -1,15 +1,28 @@
-import { describe, expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import { act, render, screen } from '@testing-library/react';
 
 import deckData from '@/data/cards';
 
 import CardDeckDetails from './CardDeckDetails';
 
 describe('Component: CardDeckDetails', () => {
-  test("should display the first card's title in the details panel", () => {
-    render(<CardDeckDetails />);
+  beforeEach(() => {
+    vi.useFakeTimers({ shouldAdvanceTime: true });
+  });
 
+  afterEach(() => {
+    vi.runOnlyPendingTimers();
+    vi.useRealTimers();
+  });
+
+  test("should display the first card's title in the details panel after the loading indicator", async () => {
+    render(<CardDeckDetails />);
     const cardData = deckData[0];
+
+    // With the loading indicator you should not be able to find anything
+    expect(screen.queryAllByText(cardData.title, { selector: 'h4' }).length).toEqual(0);
+
+    await act(() => vi.runAllTimers());
 
     expect(screen.getByText(cardData.title, { selector: 'h4' })).toBeDefined();
   });
