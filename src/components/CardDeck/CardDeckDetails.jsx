2cdfe2f3233d7cb9ccd4b9f3f3cc05a5f2fea547
Vasileios Papadopoulos <vspapadopou@gmail.com>
2025-09-09 18:18:31 +0300

    Show indicator in card grid and save button on details

--- src/components/CardDeck/CardDeckDetails.jsx
+++ src/components/CardDeck/CardDeckDetails.jsx
@@ -35,7 +35,7 @@ function CardDeckDetails() {
     <ScrollArea className="h-[calc(100vh-60px)]">
       {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
       <div className="flex justify-center px-8 py-10" tabIndex="0">
-        {loading ? <LoadingSpinner /> : <LSCard data={card} />}
+        {loading ? <LoadingSpinner /> : <LSCard data={card} canSave />}
       </div>
     </ScrollArea>
   );
